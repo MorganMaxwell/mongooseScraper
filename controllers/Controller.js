5fe9fb68b60c9ba9bd24fa76c1277a086c06d822
Morgan M <morgan.maxwell20@gmail.com>
2019-03-19 12:22:43 -0600

    able to push to database

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -2,14 +2,43 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const express = require("express");
 const mongoose = require("mongoose");
-// connect to mongodb on heroku, or do a localhost connection
+
 const MONGODB = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB, { useNewUrlParser: true });
 
 const db = require("../models");
+module.exports = function (app) {
+    app.get("/", function (req, res) {
+        axios.get("https://www.propublica.org/").then(function (response) {
+            let $ = cheerio.load(response.data);
+            $(".story-entry").each(function (i, element) {
+                let result = {};
+                result.headline = $(this)
+                    .children(".description")
+                    .children(".hed")
+                    .children("a")
+                    .text();
 
-const router = express.Router();
+                result.summary = $(this)
+                    .children(".description")
+                    .children(".dek")
+                    .text();
 
-module.exports = function(app) {
-
-}
+                result.link = $(this)
+                    .children(".description")
+                    .children(".hed")
+                    .children("a")
+                    .attr("href");
+                console.log(result);
+                db.Article.create(result)
+                    .then(function (dbArticle) {
+                        console.log(dbArticle);
+                    })
+                    .catch(function (err) {
+                        console.log(err);
+                    });
+            });
+        });
+        res.send("scraped");
+    });
+};
--- server.js
+++ server.js
@@ -1,23 +1,19 @@
-// requiring packages
 const express = require("express");
-// I have to use it, it's my own name :)
+const exphbs = require("express-handlebars");
 const logger = require("morgan");
-// initialize the express package
 const app = express();
-// tell express to use morgan logger package with status codes and such for route hits
+const PORT = process.env.PORT || 3000;
+
 app.use(logger("dev"));
-// parse req as json
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// requiring handlebars 
-const exphbs = require("express-handlebars");
-// setting handlebars to use 'main' as basic HTML
+
+app.use(express.static("public"));
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-// send the public folder to the browser
-app.use(express.static("public"));
-// declare where routes are coming from and use them
+
 require("./controllers/Controller.js")(app);
-// wait for hits on PORT 3000
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => { console.log("https://localhost:" + PORT) });
+
+app.listen(PORT, function () { console.log("http://localhost:" + PORT) });
